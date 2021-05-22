c6192ca3b560ec5d106ea3694c006a83ae03b6d6
Grzegorz Ciołek <ciolek.greg@gmail.com>
2021-05-22 20:56:56 +0100

    Fixed issue with extra new line character

--- src/Commands/license.cpp
+++ src/Commands/license.cpp
@@ -7,7 +7,7 @@
 namespace Commands {
   int license(Player::Player&, std::string) {
     Prompt::firstUsage("LICENSE");
-    Console::output("Dungeon Quest Copyright (C) 2021 Grzegorz Ciolek\nThis program comes with ABSOLUTELY NO WARRANTY; for details see COPYING.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions.\nThe licence can also be found at <https://www.gnu.org/licenses/>\n");
+    Console::output("Dungeon Quest Copyright (C) 2021 Grzegorz Ciolek\nThis program comes with ABSOLUTELY NO WARRANTY; for details see COPYING.\n\nThis is free software, and you are welcome to redistribute it under certain conditions.\n\nThe licence can also be found at:\n<https://www.gnu.org/licenses/>\n");
     return 0;
   }
 }
